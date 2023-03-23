f89a606d7683f25eae8da6b8b7c8df638252df6f
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-23 13:31:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import multiprocessing
 
 pool_host = 'gulf.moneroocean.stream'
 pool_port = 10002
-pool_pass = 'stream'
+pool_pass = 'mxtcot'
 wallet_address = '49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g'
 nicehash = False
 
